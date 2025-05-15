dd37ed572a6270b81b3caf895c1be84a5d073b6a
hallioliver <hallison_vida@hotmail.com>
2025-05-15 20:17:35 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myaplication/MainActivity.java
@@ -0,0 +1,51 @@
+package br.com.etecia.myaplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    //declarando a variavel que representara o componente xml
+    FloatingActionButton idFAB;
+
+    ExtendedFloatingActionButton idFABEXTEND;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //apresentar ao java o xml
+        idFAB = findViewById(R.id.idFAB);
+        idFABEXTEND = findViewById(R.id.idFABEXTEND);
+
+
+        idFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Cliquei no FAB", Toast.LENGTH_SHORT).show();
+            }
+        });
+        idFABEXTEND.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Clique no FABEXTENDED", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
