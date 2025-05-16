2230da74270a1672dbd108cd6373918819f714c0
hallioliver <hallison_vida@hotmail.com>
2025-05-15 21:24:05 -0300

    Tomakes Hallicon Spocilion

--- app/src/main/java/br/com/etecia/myaplication/MainActivity.java
+++ app/src/main/java/br/com/etecia/myaplication/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         idFABExtend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Clique no FABExtend", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Cliquei no FABExtend", Toast.LENGTH_SHORT).show();
             }
         });
 
